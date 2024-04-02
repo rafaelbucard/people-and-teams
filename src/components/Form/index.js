838b297928950b47f0d0c6c819b5bc4a4f5350cb
Rafael Pereira Buçard <rafaelbucard@gmail.com>
2024-04-02 17:38:17 -0300

    feat: get  form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,9 +2,10 @@ import './Form.css'
 import TextField from '../TextField';
 import DropdownList from '../DropdownList';
 import Button from '../Button';
+import { useState } from 'react';
 
 const Form = () =>{
-
+    
     const teams = [
         'Jiu-jitsu',
         'Luta livre',
@@ -15,19 +16,48 @@ const Form = () =>{
         'Box'
     ]
 
+    const [name, setName] = useState('')
+    const [role, setRole] = useState('')
+    const [image, setImage] = useState('')
+    const [team, setTeam] = useState('')
+    
+
+
     const cardSave = (event) =>{
         event.preventDefault()
-        console.log('Form foi submetido')
+        console.log('Form foi submetido: ', name, role, image, team)
     }
 
     return(
         <section className='formulario'>
             <form onSubmit={cardSave}>
                 <h1>Preencha os campos com os dados do professor</h1>
-                <TextField required={true} label = "Nome" placeholder = "Digite seu nome"/>
-                <TextField required={true} label = "Cargo-Faixa" placeholder = "Digite seu cargo"/>
-                <TextField label = "Imagem" placeholder = "Digite o endereço da imagem" />
-                <DropdownList required={true} label = "Arte Marcial" items = {teams}/>
+                <TextField
+                    required={true}
+                    label = "Nome"
+                    placeholder = "Digite seu nome"
+                    value={name}
+                    whenChange={value => setName(value)}
+                />
+                <TextField
+                    required={true}
+                    label = "Cargo-Faixa"
+                    placeholder = "Digite seu cargo"
+                    value={role}
+                    whenChange={value => setRole(value)}
+                />
+                <TextField
+                    label = "Imagem"
+                    placeholder = "Digite o endereço da imagem"
+                    value={image}
+                    whenChange={value => setImage(value)}
+                />
+                <DropdownList
+                    required={true}
+                    label = "Arte Marcial" 
+                    items = {teams}
+                    whenCnange={value => setTeam(value)}
+                />
                 <Button>
                     Criar card
                 </Button>
