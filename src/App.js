6105cd12ace01000f7fad43ab769f0612e784046
Rafael Pereira Buçard <rafaelbucard@gmail.com>
2024-04-18 17:55:59 -0300

    feat: created team compoent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Banner from './components/Banner';
 import Form from './components/Form';
+import Team from './components/Team';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
       <Form/>
+      <Team name="Jiu-Jitsu"/>
+      <Team name="Capoeira"/>
+      <Team name="Box"/>
     </div>
   );
 }
